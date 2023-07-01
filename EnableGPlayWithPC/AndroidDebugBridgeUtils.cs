a6d318e0cfce34ff9cfc4dfd8c2cc2136b65639f
Syuugo <pub@s1204.me>
2023-07-01 19:25:24 +0900

    Updates
    Add Icon

--- EnableGPlayWithPC/AndroidDebugBridgeUtils.cs
+++ EnableGPlayWithPC/AndroidDebugBridgeUtils.cs
@@ -8,11 +8,6 @@ using SharpAdbClient;
 
 namespace EnableGPlayWithPC {
     internal static class AndroidDebugBridgeUtils {
-        /// <summary>
-        /// ADBからの文字列を調べて、権限付与に成功しているかどうかチェックする。
-        /// </summary>
-        /// <param name="str">出力。</param>
-        /// <returns>権限付与に成功しているかどうか。</returns>
         internal static bool IsPermissionGranted(string str) {
             string[] lines = str.Split('\n');
             if (lines.Where(s => s.StartsWith("Operation not allowed:")).Any()) {
@@ -24,14 +19,6 @@ namespace EnableGPlayWithPC {
             }
         }
 
-        /// <summary>
-        /// 権限を付与する。
-        /// </summary>
-        /// <param name="packageName">パッケージ名。</param>
-        /// <param name="perms">権限の一覧。</param>
-        /// <param name="device">ADB デバイス。</param>
-        /// <param name="handle">ウィンドウハンドル。</param>
-        /// <returns>成功したかどうか。</returns>
         internal static bool GrantPermissions(string packageName, IEnumerable<string> perms, DeviceData device, IntPtr handle) {
             foreach (string perm in perms) {
                 ConsoleOutputReceiver receiver = new ConsoleOutputReceiver();
@@ -49,12 +36,6 @@ namespace EnableGPlayWithPC {
             return true;
         }
 
-        /// <summary>
-        /// アプリのインストール
-        /// </summary>
-        /// <param name="device">デバイスデータ</param>
-        /// <param name="fileName">ファイル名</param>
-        /// <returns>成功したかどうか。</returns>
         internal static bool InstallPackage(DeviceData device, string fileName) {
             string appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string installCmd = $"pm install -i \"com.android.vending\" -r /data/local/tmp/base.apk";
